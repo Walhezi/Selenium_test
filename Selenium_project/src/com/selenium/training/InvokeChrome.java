d5f5c40ab2eb7f031eb346d9faa9cf7624d3fc44
walhezi <walhezi.wiem@gmail.com>
2018-03-23 17:34:12 +0100

    first commit

---
+++ Selenium_project/src/com/selenium/training/InvokeChrome.java
@@ -0,0 +1,29 @@
+package com.selenium.training;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class InvokeChrome {
+	public WebDriver driver ; 
+	
+	
+	//public void webdriver() {
+	//	System.setProperty("webdriver.chrome.driver","C:\\Users\\C5259429\\Desktop\\Tools\\webdriver\\chromedriver_win32\\chromedriver.exe");
+	//	driver = new ChromeDriver();
+	//	driver.get("https://www.google.com");
+		//driver.manage().window().maximize();
+//	}
+	@Test                                 
+	public void chromeprogram() {
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\C5259429\\Desktop\\Tools\\webdriver\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.manage().window().maximize();
+		WebElement a= driver.findElement(By.linkText("Anmelden"));
+		a.click();
+		driver.quit();
+	}	
+}
